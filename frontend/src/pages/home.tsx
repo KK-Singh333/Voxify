046d45b530d1044d2566f8f6a0b8b3a04a67e8b9
Khush Kumar Singh <khushm965@gmail.com>
2025-02-11 14:51:47 +0530

    Update home.tsx

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import './home.css'
 import Cookies from 'js-cookie';
 export default function Home() {
+    console.log("Token from Cookies:", document.cookie);
     console.log(Cookies.get('token'));
     return (<div className="outer">
         <NavBar></NavBar>
