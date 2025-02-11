d05bc5a624e18e5734654c1fcb1bb47ac22fbb46
Khush Kumar Singh <khushm965@gmail.com>
2025-02-11 15:53:18 +0530

    Update signout.js

--- backend/routes/signout.js
+++ backend/routes/signout.js
@@ -3,6 +3,7 @@ const Router = express.Router();
 Router.get('/', (req, res) => {
     try{
     res.clearCookie('token');
+    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0");
     return res.json({
         errorflag:'no',
     });
