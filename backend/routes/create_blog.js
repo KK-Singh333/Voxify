c60eb7d4fd7d3b22e64edbcb763334cc525f59d4
Khush Kumar Singh <khushm965@gmail.com>
2025-02-12 19:03:19 +0530

    Update create_blog.js

--- backend/routes/create_blog.js
+++ backend/routes/create_blog.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const handleCreatedBlog=require('../controller/createdBlog')
+const handleCreatedBlog=require('../controller/createdBlog');
 const Router = express.Router();
+console.log("Came on blog route");
 Router.post('/',handleCreatedBlog)
 module.exports = Router;
